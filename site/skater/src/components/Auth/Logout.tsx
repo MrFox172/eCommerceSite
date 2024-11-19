2e30775dec8b56f05cf8b5f2338c7562eb4828f6
Zackery Perski <zperski@emich.edu>
2024-11-19 17:53:37 -0500

    Pushing small fix using useEffect for logout.

--- site/skater/src/components/Auth/Logout.tsx
+++ site/skater/src/components/Auth/Logout.tsx
@@ -1,15 +1,27 @@
 //import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Logout = () => {
-    const navigate = useNavigate();
-    
-    const handleLogout = () => {
-        localStorage.removeItem("user");
-        navigate("/");
-    }
-    return null;
+  const navigate = useNavigate();
 
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      handleLogout();
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <h1>Logout</h1>
+      <p>You will be automatically logged out in 3 seconds...</p>
+    </>
+  );
 };
 
 export default Logout;
