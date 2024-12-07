52ea9b1f3b129b3794bccfe575f7f8c4f5482bd9
Zackery Perski <zackeryperski@gmail.com>
2024-12-07 17:48:51 -0500

    Incorporated buttons and counts for cards.

--- site/skater/src/components/ProductCard/ProductCard.tsx
+++ site/skater/src/components/ProductCard/ProductCard.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Product } from "../../interfaces/products";
 import img from "../../assets/img-placeholder.svg";
 import { Card, Button } from "react-bootstrap";
 import styles from "./styles.module.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const ProductCard: React.FC<Product> = (product) => {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  const changeCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      console.log("Value changed to 0");
+      setCount(0);
+    } else {
+      console.log("Value changed to " + e.target.value);
+      setCount(parseInt(e.target.value));
+    }
+  };
+
   const navigate = useNavigate();
+
   return (
     <>
       <Card className="p-2" key={product.id}>
@@ -32,6 +56,9 @@ const ProductCard: React.FC<Product> = (product) => {
               >
                 Add to cart
               </Button>
+              <Button onClick={decrement}>-</Button>
+              <input className={styles.input}type="text" value={count} onChange={changeCount} />
+              <Button onClick={increment}>+</Button>
               <Button
                 variant="outline-secondary"
                 size="sm"
